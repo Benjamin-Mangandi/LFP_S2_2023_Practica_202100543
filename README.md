3deb4e10089b7026f65d1fcd21b2895a1a3cc860
Benjamin-Mangandi <3011042200101@ingenieria.usac.edu.gt>
2023-08-21 19:53:48 -0600

    Manual v0.5 y errores corregidos.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ### **Segundo Semestre 2023**
 
 ```js
-Universidad San Carlos de Guatemala
+Universidad San Carlos De Guatemala
 Programador: Harold Benjamin Oxlaj Mangandi
 Carne: 202100543
 ```
@@ -11,3 +11,15 @@ Carne: 202100543
 ## Descripción del Proyecto
 El siguiente Proyecto consiste en carga masiva en el lenguaje de programacion 
 python el cual simula un gestor sencillo de inventarios.
+## Partes del Proyecto
+**Menu Principal**
+El menu principal muestra las 4 distintas opciones que el usuario puede elegir en consola, *Cargar Inventario inicial*, *Cargar Movimientos en el inventario*, *Crear informe del inventario* y *salir*. dicho menu está encerrado en un bucle *while*, en el que saldrá cuando la respuesta del usuario que se guarda en la variable *respuesta_usuario* sea igual a 4; la validacion de la respuesta del usuario se ejecuta por medio de la funcion ***elegir_opcion***
+## **Funciones**
+### *elegir_opcion*
+esta funcion recibe como parametro un arreglo y la respuesta del usuario; esta funcion llama a las demas funciones dependiendo del numero ingresado por el usuario y retornando la respuesta del usuario.
+Las funciones ***cargar_inventario*** y ***cargar_movimientos** se ingresa como parametro el nombre de los archivos ingresados por el usuario por medio de la variable *nombre*
+### *convertir_numericos*
+esta funcion recibe como parametro un arreglo, el cual convierte en *int* los *str* de los precios y cantidades del inventario validando si el arreglo mandado en la funcion es un arreglo con los movimientos o la carga del inventario, arreglando tambien errores en nombres de las bodegas, guardando los datos en variables auxiliares.
+### *cargar_inventario*
+### *cargar_movimientos*
+### *crear_informe*
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-#Funcion Cargar Inventario
-materiales = []
+inventario_final = []
+
+# Funcion Cargar Inventario
+
 def convertir_numericos(datos_inventario):
     if datos_inventario[0][0] != "agregar_stock" and datos_inventario[0][0] != "vender_producto":
         for bodega in range(len(datos_inventario)):
@@ -24,7 +26,8 @@ def convertir_numericos(datos_inventario):
             cantidad_numerica = int(cantidad)
             datos_inventario[bodega][2] = cantidad_numerica
 
-def cargar_inventario(datos_inventario,nombre_archivo):
+
+def cargar_inventario(datos_inventario, nombre_archivo):
     inventario = open(nombre_archivo, 'r')
     for linea in inventario:
         aux_separar_lineas = linea.split(" ")
@@ -36,6 +39,7 @@ def cargar_inventario(datos_inventario,nombre_archivo):
     print(datos_inventario)
     return
 
+
 def cargar_movimientos(datos_inventario, nombre_archivo):
     inventario = open(nombre_archivo, 'r')
     materiales_agregar = []
@@ -44,79 +48,88 @@ def cargar_movimientos(datos_inventario, nombre_archivo):
         instruccion = aux_separar_lineas[0]
         aux_separar_datos = aux_separar_lineas[1]
         aux_datos_finales = aux_separar_datos.split(";")
-        aux_datos_finales.insert(0,instruccion)
+        aux_datos_finales.insert(0, instruccion)
         materiales_agregar.append(aux_datos_finales)
     convertir_numericos(materiales_agregar)
     print(materiales_agregar)
     for materiales in range(len(materiales_agregar)):
-            for Bodegas in range(len(datos_inventario)):
-                if materiales_agregar[materiales][0] == "agregar_stock":
-                    if datos_inventario[Bodegas][3] == materiales_agregar[materiales][3]:
-                        if datos_inventario[Bodegas][0] == materiales_agregar[materiales][1]:
-                            nueva_cantidad = datos_inventario[Bodegas][1]+materiales_agregar[materiales][2]
+        for Bodegas in range(len(datos_inventario)):
+            if materiales_agregar[materiales][0] == "agregar_stock":
+                if datos_inventario[Bodegas][3] == materiales_agregar[materiales][3]:
+                    if datos_inventario[Bodegas][0] == materiales_agregar[materiales][1]:
+                        nueva_cantidad = datos_inventario[Bodegas][1] + \
+                            materiales_agregar[materiales][2]
+                        datos_inventario[Bodegas][1] = nueva_cantidad
+                    else:
+                        print("Lo sentimos, pero el producto: " + materiales_agregar[materiales][1] +
+                              "No existe en: " + datos_inventario[Bodegas][3])
+            else:
+                if datos_inventario[Bodegas][3] == materiales_agregar[materiales][3]:
+                    if datos_inventario[Bodegas][0] == materiales_agregar[materiales][1]:
+                        nueva_cantidad = datos_inventario[Bodegas][1] - \
+                            materiales_agregar[materiales][2]
+                        if nueva_cantidad < 0:
+                            print("La venta sobrepasa los productos existentes")
+                        else:
                             datos_inventario[Bodegas][1] = nueva_cantidad
-                        else:
-                            print("Lo sentimos, pero el producto: "+ materiales_agregar[materiales][1]+ 
-                                  "No existe en: "+ datos_inventario[Bodegas][3]) 
-                else:
-                    if datos_inventario[Bodegas][3] == materiales_agregar[materiales][3]:
-                        if datos_inventario[Bodegas][0] == materiales_agregar[materiales][1]:
-                            nueva_cantidad = datos_inventario[Bodegas][1]- materiales_agregar[materiales][2]
-                            if nueva_cantidad<0:
-                                print("La venta sobrepasa los productos existentes")
-                            else:
-                                datos_inventario[Bodegas][1] = nueva_cantidad
-                        else:
-                            print("Lo sentimos, pero el producto: "+ materiales_agregar[materiales][1]+ 
-                                  " No existe en: "+ datos_inventario[Bodegas][3]) 
+                    else:
+                        print("Lo sentimos, pero el producto: " + materiales_agregar[materiales][1] +
+                              " No existe en: " + datos_inventario[Bodegas][3])
 
     print("Inventario actualizado con exito")
     return
 
+# Funcion para hacer el archivo txt
+
 def crear_informe(datos_inventario):
     archivo_inventario = open("Inventario Final.txt", "w+")
-    archivo_inventario.write("-"*80+"\n")
+    archivo_inventario.write("-"*110+"\n")
     archivo_inventario.write("Informe de Productos del inventario: \n")
-    archivo_inventario.write("Producto  ----  Cantidad  ----  Precio Unitario  "
-           "----  Valor Total  ----  Ubicacion\n")
-    archivo_inventario.write("*"*80+"\n")
+    archivo_inventario.write("\nProducto    ------    Cantidad    ------    Precio Unitario    "
+                             "------    Valor Total    ------    Ubicacion\n")
+    archivo_inventario.write("*"*110+"\n")
     for bodegas in range(len(datos_inventario)):
-        archivo_inventario.write(datos_inventario[bodegas][0].ljust(10) + "----".ljust(8)+
-              str(datos_inventario[bodegas][1]).ljust(8)+ "----".ljust(11)+
-              str(datos_inventario[bodegas][2]).ljust(12)+ "----".ljust(9)+
-              str(datos_inventario[bodegas][1]*datos_inventario[bodegas][2]).ljust(10) + 
-              "----".ljust(7)+
-              str(datos_inventario[bodegas][3])+ "\n")
+        valor_total = datos_inventario[bodegas][1] * datos_inventario[bodegas][2]
+        valor_total_cuatro_decimales = "{:.4f}".format(valor_total)
+        archivo_inventario.write(datos_inventario[bodegas][0].ljust(12) + "------".ljust(13) +
+                                 str(datos_inventario[bodegas][1]).ljust(9) + "------".ljust(15) +
+                                 "Q"+ str(datos_inventario[bodegas][2]).ljust(13) + "------".ljust(12) +
+                                 "Q"+ str(valor_total_cuatro_decimales).ljust(12) +
+                                 "------".ljust(11) +
+                                 str(datos_inventario[bodegas][3]) + "\n")
+    print("\n¡Informe Creado con exito!")
     return
 
 
-#Funcion Para elegir las opciones
-def elegir_opcion(numero, datos):
-    if numero == 1:
-        nombre = input("Ingrese el Nombre del archivo con su extensión: ")
-        cargar_inventario(datos, nombre)
-        return
-    elif numero == 2:
-        nombre = input("Ingrese el Nombre del archivo con su extensión: ")
-        cargar_movimientos(datos, nombre)
-        return numero
-    elif numero == 3:
-        crear_informe(datos)
-        return numero
-    elif numero == 4:
-        print("¡Hasta Luego!")
-        return numero
+# Funcion para elegir las opciones
 
-#Inicio de la ejecución
-respuesta = 0
-while respuesta != 4:
-    print("  ")
-    print("--------Practica 1 Lenguajes Formales y de programación-------")
+def elegir_opcion(respuesta_usuario, inventario):
+    if respuesta_usuario == 1:
+        nombre = input("Ingrese el Nombre del archivo con su extensión: ")
+        cargar_inventario(inventario, nombre)
+        return respuesta_usuario
+    elif respuesta_usuario == 2:
+        nombre = input("Ingrese el Nombre del archivo con su extensión: ")
+        cargar_movimientos(inventario, nombre)
+        return respuesta_usuario
+    elif respuesta_usuario == 3:
+        crear_informe(inventario)
+        return respuesta_usuario
+    elif respuesta_usuario == 4:
+        print("\n¡Hasta Luego!")
+        return respuesta_usuario
+
+
+# Inicio de la ejecución
+
+respuesta_usuario = 0
+while respuesta_usuario != 4:
+    print("\n--------Practica 1 Lenguajes formales y de programación-------")
     print("Sistema de Inventario:")
-    print("Para Acceder a cada una, ingrese el numero correspondiente.")
+    print("Para Acceder a cada opcion, ingrese el numero correspondiente.")
     print("1. Cargar Inventario Inicial")
     print("2. Cargar instrucciones de movimientos")
     print("3. Crear informe de inventario")
     print("4. Salir")
-    respuesta = int(input("Ingresa a una opción: "))
-    elegir_opcion(respuesta, materiales)
+    respuesta_usuario = int(input("Ingresa a una opción: "))
+    elegir_opcion(respuesta_usuario, inventario_final)
