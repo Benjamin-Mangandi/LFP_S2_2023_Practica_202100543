f518ccc0ef8cb100923b8cf5659f8558bf731e71
Benjamin-Mangandi <3011042200101@ingenieria.usac.edu.gt>
2023-08-21 21:51:29 -0600

    Manual v0.8 y otros errores corregidos.

--- main.py
+++ main.py
@@ -2,29 +2,37 @@ inventario_final = []
 
 # Funcion Cargar Inventario
 
+
 def convertir_numericos(datos_inventario):
+    # Arreglo con movimientos
     if datos_inventario[0][0] != "agregar_stock" and datos_inventario[0][0] != "vender_producto":
-        for bodega in range(len(datos_inventario)):
-            nombre_error = datos_inventario[bodega][3]
+        # Corrigiendo nombres
+        for numero_bodega in range(len(datos_inventario)):
+            nombre_error = datos_inventario[numero_bodega][3]
             nombre_sin_error = nombre_error.replace("\n", "")
-            datos_inventario[bodega][3] = nombre_sin_error
-        for bodega in range(len(datos_inventario)):
-            cantidad = datos_inventario[bodega][1]
+            datos_inventario[numero_bodega][3] = nombre_sin_error
+        # Convirtiendo a int cantidad
+        for numero_bodega in range(len(datos_inventario)):
+            cantidad = datos_inventario[numero_bodega][1]
             cantidad_numerica = int(cantidad)
-            datos_inventario[bodega][1] = cantidad_numerica
-        for bodega in range(len(datos_inventario)):
-            precio = datos_inventario[bodega][2]
+            datos_inventario[numero_bodega][1] = cantidad_numerica
+        # Convirtiendo a float precio
+        for numero_bodega in range(len(datos_inventario)):
+            precio = datos_inventario[numero_bodega][2]
             precio_numerico = float(precio)
-            datos_inventario[bodega][2] = precio_numerico
+            datos_inventario[numero_bodega][2] = precio_numerico
     else:
-        for bodega in range(len(datos_inventario)):
-            nombre_error = datos_inventario[bodega][3]
+        # Inventario
+        # Corrigiendo nombres
+        for numero_bodega in range(len(datos_inventario)):
+            nombre_error = datos_inventario[numero_bodega][3]
             nombre_sin_error = nombre_error.replace("\n", "")
-            datos_inventario[bodega][3] = nombre_sin_error
-        for bodega in range(len(datos_inventario)):
-            cantidad = datos_inventario[bodega][2]
+            datos_inventario[numero_bodega][3] = nombre_sin_error
+        # Convirtiendo a int cantidad
+        for numero_bodega in range(len(datos_inventario)):
+            cantidad = datos_inventario[numero_bodega][2]
             cantidad_numerica = int(cantidad)
-            datos_inventario[bodega][2] = cantidad_numerica
+            datos_inventario[numero_bodega][2] = cantidad_numerica
 
 
 def cargar_inventario(datos_inventario, nombre_archivo):
@@ -57,29 +65,33 @@ def cargar_movimientos(datos_inventario, nombre_archivo):
             if materiales_agregar[materiales][0] == "agregar_stock":
                 if datos_inventario[Bodegas][3] == materiales_agregar[materiales][3]:
                     if datos_inventario[Bodegas][0] == materiales_agregar[materiales][1]:
-                        nueva_cantidad = datos_inventario[Bodegas][1] + \
-                            materiales_agregar[materiales][2]
+                        nueva_cantidad = datos_inventario[Bodegas][1] + materiales_agregar[materiales][2]
                         datos_inventario[Bodegas][1] = nueva_cantidad
+                        break
                     else:
                         print("Lo sentimos, pero el producto: " + materiales_agregar[materiales][1] +
-                              "No existe en: " + datos_inventario[Bodegas][3])
-            else:
+                        "No existe en: " + datos_inventario[Bodegas][3])
+                        break
+            elif materiales_agregar[materiales][0] == "vender_producto":
                 if datos_inventario[Bodegas][3] == materiales_agregar[materiales][3]:
                     if datos_inventario[Bodegas][0] == materiales_agregar[materiales][1]:
-                        nueva_cantidad = datos_inventario[Bodegas][1] - \
-                            materiales_agregar[materiales][2]
+                        nueva_cantidad = datos_inventario[Bodegas][1] - materiales_agregar[materiales][2]
                         if nueva_cantidad < 0:
                             print("La venta sobrepasa los productos existentes")
+                            break
                         else:
                             datos_inventario[Bodegas][1] = nueva_cantidad
+                            break
                     else:
                         print("Lo sentimos, pero el producto: " + materiales_agregar[materiales][1] +
-                              " No existe en: " + datos_inventario[Bodegas][3])
+                        " No existe en: " + datos_inventario[Bodegas][3])
+                        break
 
     print("Inventario actualizado con exito")
     return
 
 # Funcion para hacer el archivo txt
+
 
 def crear_informe(datos_inventario):
     archivo_inventario = open("Inventario Final.txt", "w+")
@@ -88,15 +100,18 @@ def crear_informe(datos_inventario):
     archivo_inventario.write("\nProducto    ------    Cantidad    ------    Precio Unitario    "
                              "------    Valor Total    ------    Ubicacion\n")
     archivo_inventario.write("*"*110+"\n")
-    for bodegas in range(len(datos_inventario)):
-        valor_total = datos_inventario[bodegas][1] * datos_inventario[bodegas][2]
+    for numero_bodega in range(len(datos_inventario)):
+        valor_total = datos_inventario[numero_bodega][1] * datos_inventario[numero_bodega][2]
         valor_total_cuatro_decimales = "{:.4f}".format(valor_total)
-        archivo_inventario.write(datos_inventario[bodegas][0].ljust(12) + "------".ljust(13) +
-                                 str(datos_inventario[bodegas][1]).ljust(9) + "------".ljust(15) +
-                                 "Q"+ str(datos_inventario[bodegas][2]).ljust(13) + "------".ljust(12) +
-                                 "Q"+ str(valor_total_cuatro_decimales).ljust(12) +
+        archivo_inventario.write(datos_inventario[numero_bodega][0].ljust(12) +
+                                 "------".ljust(13) +
+                                 str(datos_inventario[numero_bodega][1]).ljust(9) +
+                                 "------".ljust(15) +
+                                 "Q" + str(datos_inventario[numero_bodega][2]).ljust(13) +
+                                 "------".ljust(12) +
+                                 "Q" + str(valor_total_cuatro_decimales).ljust(12) +
                                  "------".ljust(11) +
-                                 str(datos_inventario[bodegas][3]) + "\n")
+                                 str(datos_inventario[numero_bodega][3]) + "\n")
     print("\n¡Informe Creado con exito!")
     return
 
@@ -105,17 +120,21 @@ def crear_informe(datos_inventario):
 
 def elegir_opcion(respuesta_usuario, inventario):
     if respuesta_usuario == 1:
+        # Cargar Inventario
         nombre = input("Ingrese el Nombre del archivo con su extensión: ")
         cargar_inventario(inventario, nombre)
         return respuesta_usuario
     elif respuesta_usuario == 2:
+        # Cargar Movimientos
         nombre = input("Ingrese el Nombre del archivo con su extensión: ")
         cargar_movimientos(inventario, nombre)
         return respuesta_usuario
     elif respuesta_usuario == 3:
+        # Crear Informe
         crear_informe(inventario)
         return respuesta_usuario
     elif respuesta_usuario == 4:
+        # Salir
         print("\n¡Hasta Luego!")
         return respuesta_usuario
 
